8013a10048bda82937bb17c6513e94243a2c09a7
CristobalHdz <cristobalhdzch@gmail.com>
2021-09-21 16:34:41 -0600

    Add Category code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,29 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    productId: {
+      TYPE: DataTypes.INTEGER,
+      references: {
+        keu: 'id',
+        model: 'product'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        keu: 'id',
+        model: 'product'
+      }
+    }
   },
   {
     sequelize,
@@ -16,5 +34,6 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
+
 
 module.exports = ProductTag;
